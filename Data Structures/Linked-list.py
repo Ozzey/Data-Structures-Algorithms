acb740bd7a030b0371ece1e075e30d6cff07deb0
Aditya Narendra <49760167+Ozzey@users.noreply.github.com>
2023-10-19 21:47:40 +0300

    Update Linked-list.py

--- Data Structures/Linked-list.py
+++ Data Structures/Linked-list.py
@@ -1,16 +1,11 @@
-class Node:
-    def __init__(self,data):
-        self.data = data
+class ListNode:
+    def __init__(self, val):
+        self.val = val
         self.next = None
 
-# Time Complexity O(N)
-def printLL(head : int) -> None:
-    while head is not None:
-        print(head.data)
-        head = head.next
 
 # Time Complexity: O(N)
-def takeInput() -> Node:
+def takeInput() -> ListNode:
     dataList = [int(nums) for nums in input().split() if nums != "-1"]
 
     head = None
@@ -19,7 +14,7 @@ def takeInput() -> Node:
     # Iterate through the list
     for currData in dataList:
         # Creating new node in the linkedlist
-        newNode = Node(currData)
+        newNode = ListNode(currData)
 
         # Assigning new node as head if head is none
         if head is None:
@@ -35,8 +30,49 @@ def takeInput() -> Node:
     return head
 
 
+# Time Complexity O(N)
+def printLL(head: ListNode) -> None:
+    out = ""
+    while head is not None:
+        out += f'{head.val}'
+        out += " -> "
+        head = head.next
+    print(out[:-4])
+
+
+# Time Complexity: O(N)
+def inputLL(dataList):
+    head = None
+    tail = None
+
+    # Iterate through the list
+    for currData in dataList:
+        # Creating new node in the linkedlist
+        newNode = ListNode(currData)
+
+        # Assigning new node as head if head is none
+        if head is None:
+            head = newNode
+            # Assigning most recent node as the tail
+            tail = newNode
+
+        # if head is not none
+        else:
+            tail.next = newNode
+            tail = newNode
+    return head
+
+
+def copy(head):
+    arr = []
+    while head:
+        arr.append(head.val)
+        head = head.next
+    return inputLL(arr)
+
+
 # Time Complexity : O(N)
-def lengthLL(head : int) -> int:
+def lengthLL(head: ListNode) -> int:
     length = 0
     while head is not None:
         length += 1
@@ -45,7 +81,7 @@ def lengthLL(head : int) -> int:
 
 
 # Time Complexity : O(i)
-def getNode(head : int, index : int) -> Node:
+def getNode(head: ListNode, index: int) -> ListNode:
     ind = 0
     while head is not None:
         if ind == index:
@@ -53,15 +89,16 @@ def getNode(head : int, index : int) -> Node:
         else:
             ind += 1
             head = head.next
-    return Exception("Index of out range")
+    raise Exception("Index of out range")
 
 
 # Time Complexity : O(1)
-def insertNodeAtHead(head : int ,data : int) -> Node:
-    newNode = Node(data)
+def insertNodeAtHead(head: ListNode, data: int) -> ListNode:
+    newNode = ListNode(data)
     if head:
-       newNode.next = head
+        newNode.next = head
     return newNode
+
 
 # Time Complexity : O(n)
 def insert(head, index, data):
@@ -78,7 +115,7 @@ def insert(head, index, data):
 
     else:
         ind = 0  # Initialize an index counter
-        newNode = Node(data)  # Create a new node with the given data
+        newNode = ListNode(data)  # Create a new node with the given data
 
         # Traverse the linked list
         while head is not None:
@@ -94,5 +131,101 @@ def insert(head, index, data):
                 ind += 1  # Increment the index counter
 
 
-# head = takeInput()
-# head = insert(head,5,100)
+# Time Complexity : O(N)
+def insertRec(head, index, data):
+    if index < 0:
+        raise ValueError("Index out of range")
+
+    if index == 0:
+        newNode = ListNode(data)
+        newNode.next = head
+        return newNode
+
+    if head is None:
+        raise ValueError("Index out of range")
+
+    tempHead = insertRec(head.next, index - 1, data)
+    head.next = tempHead
+    return head
+
+
+# Time Complexity: O(N)
+def deleteNode(head, index):
+    idx = 0
+    prev = None
+    curr = head
+    next = head.next
+
+    if index < 0:
+        raise Exception("Index of out range")
+
+    # 1 -> 2 -> 3 -> 4
+    while head is not None:
+        if index == 0:
+            return next
+        elif idx == index:
+            prev.next = next
+            return curr
+        else:
+            idx += 1
+            prev = head
+            head = head.next
+            try :
+                next = head.next
+            except Exception as e:
+                raise ValueError("Index out of range")
+
+
+# Time Complexity: O(N)
+def deleteRec(head,index):
+
+    if index < 0:
+        raise ValueError("Index out of range")
+
+    if index == 0:
+        return head.next
+    tempNode = deleteNode(head.next, index - 1)
+
+    if head is None:
+        return ValueError("Index out of range")
+
+    head.next = tempNode
+    return head
+
+
+# Time Complexity: O(N)
+def reverseLL(head):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
+
+    current = head
+    prev = None
+
+    while current is not None:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+
+# Time Complexity: O(N)
+def deleteElement(head, value):
+    prev = None
+    first = head
+
+    while head and head.val == value:
+        head = head.next
+        first = head
+
+    while head:
+        if head.val == value:
+            prev.next = head.next
+            head = head.next
+        else:
+            prev = head
+            head = head.next
+
+    return first
